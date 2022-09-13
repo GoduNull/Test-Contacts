eeee9c296cf8b4ff0e95f4bd044c61356edc3f7e
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-09-13 17:09:42 +0300

    feat: add contact model and model configuration

---
+++ src/Test-Contacts.Data/Configurations/ContactConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_Contacts.Data.Models;
+
+namespace Test_Contacts.Data.Configurations
+{
+    class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder = builder ?? throw new ArgumentNullException(nameof(builder));
+
+            builder.Property(contact => contact.Id)
+                .UseIdentityColumn();
+
+            builder.Property(contact => contact.Name)
+                .IsRequired()
+                .HasMaxLength(127);
+
+            builder.Property(contact => contact.MobilePhone)
+                .IsRequired()
+                .HasMaxLength(127);
+
+            builder.Property(contact => contact.JobTitle)
+                .IsRequired()
+                .HasMaxLength(127);
+
+            builder.Property(contact => contact.BirthDate)
+                .IsRequired()
+                .HasColumnType("datetime");
+        }
+    }
+}
---
+++ src/Test-Contacts.Data/Contexts/ApplicationContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_Contacts.Data.Configurations;
+using Test_Contacts.Data.Models;
+
+namespace Test_Contacts.Data.Contexts
+{
+    class ApplicationContext: DbContext
+    {
+        /// <summary>
+        /// Contructor with params.
+        /// </summary>
+        /// <param name="options">Database context options.</param>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// DbSet for Contacts.
+        /// </summary>
+        public DbSet<Contact> Contacts { get; set; }
+
+        /// <summary>
+        /// Model configuration.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder = builder ?? throw new ArgumentNullException(nameof(builder));
+            builder.ApplyConfiguration(new ContactConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
