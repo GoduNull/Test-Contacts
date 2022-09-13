28a9df5e9dd16e8b20d701ecb1ed530e3eb7ff47
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-09-13 19:45:00 +0300

    fix: change Irepository

--- src/Test-Contacts.Logic/Interfaces/IRepositoryManager.cs
+++ src/Test-Contacts.Logic/Interfaces/IRepositoryManager.cs
@@ -11,74 +11,68 @@ namespace Test_Contacts.Logic.Interfaces
     /// Generic repository provide all base needed methods (CRUD).
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IRepositoryManager
+    public interface IRepositoryManager<T> where T : class
     {
         /// <summary>
-        /// Generic repository provide all base needed methods (CRUD).
+        /// Create new entity async.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        public interface IRepositoryManager<T> where T : class
-        {
-            /// <summary>
-            /// Create new entity async.
-            /// </summary>
-            /// <param name="entity">Entity object</param>
-            Task CreateAsync(T entity);
+        /// <param name="entity">Entity object</param>
+        Task CreateAsync(T entity);
 
-            /// <summary>
-            /// Create new entities async.
-            /// </summary>
-            /// <param name="entities">Entity collection.</param>
-            Task CreateRangeAsync(IEnumerable<T> entities);
+        /// <summary>
+        /// Create new entities async.
+        /// </summary>
+        /// <param name="entities">Entity collection.</param>
+        Task CreateRangeAsync(IEnumerable<T> entities);
 
-            /// <summary>
-            /// Get all queries.
-            /// </summary>
-            /// <returns>IQueryable queries.</returns>
-            IQueryable<T> GetAll();
+        /// <summary>
+        /// Get all queries.
+        /// </summary>
+        /// <returns>IQueryable queries.</returns>
+        IQueryable<T> GetAll();
 
-            /// <summary>
-            /// GetAllAsTracking
-            /// </summary>
-            /// <returns>IQueryable queries.</returns>
-            IQueryable<T> GetAllAsTracking();
+        /// <summary>
+        /// GetAllAsTracking
+        /// </summary>
+        /// <returns>IQueryable queries.</returns>
+        IQueryable<T> GetAllAsTracking();
 
-            /// <summary>
-            /// Get entity async by predicate.
-            /// </summary>
-            /// <param name="predicate">LINQ predicate.</param>
-            /// <returns>T entity.</returns>
-            Task<T> GetEntityAsync(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// Get entity async by predicate.
+        /// </summary>
+        /// <param name="predicate">LINQ predicate.</param>
+        /// <returns>T entity.</returns>
+        Task<T> GetEntityAsync(Expression<Func<T, bool>> predicate);
 
-            /// <summary>
-            /// Get entity async by predicate (without tracking).
-            /// </summary>
-            /// <param name="predicate">LINQ predicate.</param>
-            /// <returns>T entity.</returns>
-            Task<T> GetEntityWithoutTrackingAsync(Expression<Func<T, bool>> predicate);
+        /// <summary>
+        /// Get entity async by predicate (without tracking).
+        /// </summary>
+        /// <param name="predicate">LINQ predicate.</param>
+        /// <returns>T entity.</returns>
+        Task<T> GetEntityWithoutTrackingAsync(Expression<Func<T, bool>> predicate);
 
-            /// <summary>
-            /// Update entity
-            /// </summary>
-            /// <param name="entity">Entity object</param>
-            void Update(T entity);
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity object</param>
+        void Update(T entity);
 
-            /// <summary>
-            /// Remove entity from database.
-            /// </summary>
-            /// <param name="entity">Entity object.</param>
-            void Delete(T entity);
+        /// <summary>
+        /// Remove entity from database.
+        /// </summary>
+        /// <param name="entity">Entity object.</param>
+        void Delete(T entity);
 
-            /// <summary>
-            /// Remove entities from database
-            /// </summary>
-            /// <param name="entity">Entity object</param>
-            void DeleteRange(IEnumerable<T> entity);
+        /// <summary>
+        /// Remove entities from database
+        /// </summary>
+        /// <param name="entity">Entity object</param>
+        void DeleteRange(IEnumerable<T> entity);
 
-            /// <summary>
-            /// Persists all updates to the data source async.
-            /// </summary>
-            Task SaveChangesAsync();
-        }
+        /// <summary>
+        /// Persists all updates to the data source async.
+        /// </summary>
+        Task SaveChangesAsync();
     }
 }
+
