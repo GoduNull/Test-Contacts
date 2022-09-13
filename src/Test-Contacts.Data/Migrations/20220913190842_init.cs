88691f0d0528d05f7a88534d9dbb8a30dffc93c7
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-09-13 22:13:53 +0300

    feat: add migration and update database

---
+++ src/Test-Contacts.Data/Migrations/20220913190842_init.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test_Contacts.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    MobilePhone = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    JobTitle = table.Column<string>(type: "nvarchar(127)", maxLength: 127, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
