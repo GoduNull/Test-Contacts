fbcf9112723b83bc23dd28c3eb16b025d0518259
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-09-15 16:32:28 +0300

    feat: add contact view and modal view

---
+++ src/Test-Contacts.Web/wwwroot/js/getproject.js
@@ -0,0 +1,39 @@
+﻿$(function () {
+
+    //Optional: turn the chache off
+    $.ajaxSetup({ cache: false });
+
+    $('#btnCreate').click(function () {
+        $('#dialogContent').load(this.href, function () {
+            $('#dialogDiv').modal({
+                backdrop: 'static',
+                keyboard: true
+            }, 'show');
+            bindForm(this);
+        });
+        return false;
+    });
+});
+function bindForm(dialog) {
+    $('form', dialog).submit(function () {
+        if (validate()) {
+            $.ajax({
+                url: this.action,
+                type: this.method,
+                data: $(this).serialize(),
+                success: function (result) {
+                    if (result.success) {
+                        $('#dialogDiv').modal('hide');
+                    } else {
+                        $('#dialogContent').html(result);
+                        bindForm();
+                    }
+                }
+            });
+            return false;
+        }
+        else {
+            return false;
+        }
+    });
+};
